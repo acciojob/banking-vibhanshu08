5de258b28acc67579df4a3c1e3978d087585e790
vibhanshu08 <108286891+vibhanshu08@users.noreply.github.com>
2023-06-17 22:37:34 +0530

    Update StudentAccount.java

--- src/main/java/com/driver/StudentAccount.java
+++ src/main/java/com/driver/StudentAccount.java
@@ -6,7 +6,15 @@ public class StudentAccount extends BankAccount{
 
     public StudentAccount(String name, double balance, String  institutionName) {
         //minimum balance is 0 by default
-
+        super(name,balance,0);
+        this.institutionName = institutionName;
     }
 
+    public String getInstitutionName() {
+        return institutionName;
+    }
+
+    public void setInstitutionName(String institutionName) {
+        this.institutionName = institutionName;
+    }
 }
