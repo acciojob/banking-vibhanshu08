cdd2e546d1b622e5039cf501de604c35c508fe8f
vibhanshu08 <108286891+vibhanshu08@users.noreply.github.com>
2023-06-17 22:34:25 +0530

    Update BankAccount.java

--- src/main/java/com/driver/BankAccount.java
+++ src/main/java/com/driver/BankAccount.java
@@ -7,25 +7,81 @@ public class BankAccount {
     private double minBalance;
 
     public BankAccount(String name, double balance, double minBalance) {
+        this.name = name;
+        this.balance = balance;
+        this.minBalance = minBalance;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public double getMinBalance() {
+        return minBalance;
+    }
+
+    public void setMinBalance(double minBalance) {
+        this.minBalance = minBalance;
     }
 
     public String generateAccountNumber(int digits, int sum) throws Exception{
         //Each digit of an account number can lie between 0 and 9 (both inclusive)
         //Generate account number having given number of 'digits' such that the sum of digits is equal to 'sum'
         //If it is not possible, throw "Account Number can not be generated" exception
+        if (digits <= 0) {
+            throw new IllegalArgumentException("Number of digits should be positive.");
+        }
 
-        return null;
+        if (sum < 0 || sum > digits * 9) {
+            throw new Exception("Account Number cannot be generated with the given sum.");
+        }
+
+        StringBuilder accountNumber = new StringBuilder();
+
+        for (int i = 0; i < digits; i++) {
+            int digit = 0;
+
+            // To ensure the sum constraint is satisfied, we need to adjust the digit value
+            if (i == digits - 1) {
+                digit = sum;
+            } else if (sum > 9) {
+                digit = 9;
+                sum -= 9;
+            } else {
+                digit = sum;
+                sum = 0;
+            }
+
+            accountNumber.append(digit);
+        }
+
+        return accountNumber.toString();
+
     }
 
     public void deposit(double amount) {
         //add amount to balance
-
+        this.balance += amount;
     }
 
     public void withdraw(double amount) throws Exception {
         // Remember to throw "Insufficient Balance" exception, if the remaining amount would be less than minimum balance
-
+        if(this.balance-amount < this.minBalance){
+            throw new InsufficientBalanceException();
+        }
+        this.balance -= amount;
     }
 
 }
